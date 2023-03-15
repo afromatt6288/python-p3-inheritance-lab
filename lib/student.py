8c2be38300147e51b374d2b3c3bd9b294c58aa71
Matthew Clark <afromatt6288@hotmail.com>
2023-03-15 10:13:06 -0600

    Lab Complete

--- lib/student.py
+++ lib/student.py
@@ -2,7 +2,15 @@
 
 from user import User
 
+knowledge = []
+new_information = "New information"
+
 class Student(User):
+    def __init__(self, first_name, last_name,):        
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
     
-    def learn(self):
+    def learn(self, new_informaton):
+        self.new_information = new_information
+        self.knowledge.append(self.new_information)
         pass
